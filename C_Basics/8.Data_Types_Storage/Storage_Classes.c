8fc57eda34d4cd9feb41434a15b86ac6bd557703
39sanskar <mishrasanskar496@gmail.com>
2025-07-20 23:29:33 +0530

    storage

---
+++ C_Basics/8.Data_Types_Storage/Storage_Classes.c
@@ -0,0 +1,18 @@
+/*
+Storage Classes
+
+1. Automatic(auto)
+2. Static (static)
+3. Register(register) it is present inside the processor
+4. External(extern)
+
+1. Lifetime Management: Storage classes determine the lifetime (scope and duration) of variables.
+
+2. Scope Control: Dictate where a variable can be accessed, either within a function/block or throughout the program.
+
+3. Memory Location: Influence where the variable is stored, such as in the stack, heap, or data segment.
+
+4. Initialization: Define default initial values, like zero for static variables.
+*/
+
+
