25873ca7ac77f3a689165409368308db594e0854
39sanskar <mishrasanskar496@gmail.com>
2025-07-20 23:40:41 +0530

    automatic-variable

--- C_Basics/8.Data_Types_Storage/auto.c
+++ C_Basics/8.Data_Types_Storage/auto.c
@@ -12,3 +12,12 @@ int main(){
   // function() has finished; autoVar is no longer accessible
   return 0;
 }
+
+/*
+1. Default Class: Variables within a function are auto by default if no other storage class
+specifier is provided.
+2. Stack Storage: auto variables are typically stored on the stack.
+3. Function Scope: They are local to the block in which they are defined.
+4. No Initial Value: Typically have an undefined initial value unless explicitly initialized.
+5. Lifetime: Created when block is entered and destroyed when it is exited.
+*/
