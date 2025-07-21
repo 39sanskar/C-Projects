7959919ad587ec15de791d1a38a51bcf5bd3cd14
39sanskar <mishrasanskar496@gmail.com>
2025-07-21 22:14:22 +0530

    array-traversal

---
+++ C_Basics/9.Arrays/array_traversal.c
@@ -0,0 +1,19 @@
+/*
+1. Orderly Visit: Go through elements from start to end.
+2. Loops: Use for or while for access.
+3. Indexes: Start at 0, end at size - 1.
+4. Modify or Read: Perform operations on elements.
+5. Pointer Option: Increment pointers to navigate.
+*/
+
+#include <stdio.h>
+int main(){
+  int arr_size = 5;
+  int marks[5] = {90, 94, 91, 96, 100};
+
+  for (int i = 0; i < arr_size; i++){
+    printf("Marks of Student %d are: %d\n", (i + 1), marks[i]);
+  }
+  return 0;
+
+}
