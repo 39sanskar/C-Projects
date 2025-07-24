4ec171cc1200a3adddbe98a17d9733bfb87bb918
39sanskar <mishrasanskar496@gmail.com>
2025-07-24 17:17:42 +0530

    2D_array_character

--- C_Basics/10.Strings/2D_array_character.c
+++ C_Basics/10.Strings/2D_array_character.c
@@ -20,3 +20,16 @@ int main(){
   printf("Second character of the third name: %c\n", names[2][2]);
   return 0;
 }
+
+/*
+1. Matrix of Characters: In a 2D array of characters each row represents a string.
+2. String Array: Often used to store multiple strings of the same maximum length.
+3. Null Termination: Each string in the array must end with a null character '\0’.
+4. Fixed Width: All strings have a fixed maximum width, defined by the column size of the 2D array.
+5. Row Access: Access a specific string (row) with single indexing, 
+e.g., charArray[0] accesses the first string.
+6. Character Access: Access individual characters with double indexing, e.g., charArray[1][2] 
+accesses the 3rd character of the 2nd string.
+7. Initialization: Can be initialized row by row using string literals,
+e.g., char charArray[3][4] = {"abc", "def", "ghi"};.
+*/
